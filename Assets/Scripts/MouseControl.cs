c25231a6ac35523afe820f0074d3d03d8be16a7f
savior <saviorium@gmail.com>
2019-10-04 22:56:35 +0300

    Add test unit

---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+
+    private Vector3 moveTarget = Vector3.zero;
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            moveTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moveTarget.Scale(new Vector3(1f, 1f, 0f));
+        }
+
+        Vector3 travelVector = moveTarget - transform.position;
+        if(travelVector.magnitude > 0.1f) {            
+            transform.Translate(travelVector.normalized * moveSpeed * Time.deltaTime);
+        }
+    }
+}
