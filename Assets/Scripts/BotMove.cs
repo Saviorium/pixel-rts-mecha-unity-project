19667ec531942fe4ce56f337313e1c173c4d6a06
sawa9889 <cfif.sawa@bk.ru>
2019-10-08 23:49:57 +0300

    My first bots, thy moving!

--- Assets/Scripts/BotMove.cs
+++ Assets/Scripts/BotMove.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class BotMove : MonoBehaviour
 {
+    public float head;
+    public float body;
+    public float legs;
+
+    public string name;
+    public bool isChoosed;
+
+    public float moveSpeed = 20f;
+    private Vector3 moveTarget = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        head = 100;
+        body = 100;
+        legs = 100;
+
+        name = "Bot-4000";
+        isChoosed = false;
     }
 
-    // Update is called once per frame
+    void OnMouseDown ()
+    {
+        isChoosed = !isChoosed;
+        Debug.Log(isChoosed);
+    }
+
     void Update()
     {
-        
+        if ((Input.GetMouseButtonDown(1)) && (isChoosed)) {
+            moveTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moveTarget.Scale(new Vector3(1f, 1f, 0f));
+        }
+
+        Vector3 travelVector = moveTarget - transform.position;
+        if(travelVector.magnitude > 0.1f) {            
+            transform.Translate(travelVector.normalized * moveSpeed * Time.deltaTime);
+        }
     }
 }
---
+++ Assets/Scripts/building.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class building : MonoBehaviour
+{
+    private float health;
+    private string name;
+    public GameObject myPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        name = "Base";
+        Debug.Log(GetComponent<BoxCollider2D>().size);
+    }
+
+    void OnMouseDown ()
+    {
+        Instantiate(myPrefab, transform.position + new Vector3(GetComponent<BoxCollider2D>().size.x*0.5f + 0.5f, 0, 0), Quaternion.identity);
+    }
+}
