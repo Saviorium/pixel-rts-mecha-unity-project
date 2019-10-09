b9db6b918300c6f612807cb795ae62525b0922b4
savior <saviorium@gmail.com>
2019-10-10 02:04:20 +0300

    Housekeeping, refactoring

--- Assets/Scripts/BotMove.cs
+++ Assets/Scripts/BotMove.cs
@@ -18,7 +18,6 @@ public class BotMove : MonoBehaviour
 
     public Sprite selectionBorderImage;
 
-    // Start is called before the first frame update
     void Start()
     {
         InitSelectionBorder();
