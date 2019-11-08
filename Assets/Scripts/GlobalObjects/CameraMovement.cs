cacd7cdeec12d85a85d072e00249c5eca436d2be
savior <saviorium@gmail.com>
2019-11-09 02:25:15 +0300

    Add zoom to mouse pos
    Fix scroll bug, if camera size is negative

--- Assets/Scripts/GlobalObjects/CameraMovement.cs
+++ Assets/Scripts/GlobalObjects/CameraMovement.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public float zoomPerScroll = 2f; // more -> faster scroll, 1 -> no scroll, 1.1 -> slow scroll
+    private new Camera camera;
 
-   void FixedUpdate ()
+    void Start() {
+        camera = GetComponent<Camera>();
+    }
+
+    void FixedUpdate ()
     {
-        float Horizontal_x = Input.GetAxis("Horizontal");
-        float Vertical_z = Input.GetAxis("Vertical");
-        float Scroll = Input.GetAxis("Mouse ScrollWheel");
-        
-        GetComponent<Camera>().orthographicSize  += Scroll;
-        gameObject.transform.position = new Vector3( gameObject.transform.position.x + Horizontal_x, 
-                                                     gameObject.transform.position.y - Vertical_z ,
+        float horizontal_x = Input.GetAxis("Horizontal");
+        float vertical_z = Input.GetAxis("Vertical");
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if(scroll != 0) {
+            ZoomTo(getMousePos(), scroll);
+        }
+        gameObject.transform.position = new Vector3( gameObject.transform.position.x + horizontal_x, 
+                                                     gameObject.transform.position.y - vertical_z ,
                                                      -10);
+    }
+
+    private void ZoomTo(Vector3 toPoint, float scroll) {
+        // unity scrolls 0.1 by default
+        Vector3 oldPos = gameObject.transform.position;
+        float zoom = Mathf.Pow(zoomPerScroll, -scroll);
+        Vector3 newPos = oldPos + ((toPoint - oldPos) * (1 - zoom));
+        CenterScreen(newPos);
+        camera.orthographicSize = Mathf.Max(camera.orthographicSize * zoom, 0.1f); // fixme: glitched UI if max zoomed at position far from center
+    }
+
+    private void CenterScreen(Vector3 pos) {
+        gameObject.transform.position = pos;
+    }
+
+    private Vector3 getMousePos() { //TODO: utils class?
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.Scale(new Vector3(1f, 1f, 0f));
+        return pos;
     }
 }
